8a6887b52ba40fc955173f8e1d79b5c24655d316
Mfields390 <82343929+Mfields390@users.noreply.github.com>
2021-04-18 20:36:02 -0400

    Capstone week 1 additions

---
+++ VacationBudgetPlanner/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace VacationBudgetPlanner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Vacation Budget Planner!");
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello {name}. Where would you like to travel to Mexico or Jamaica?");
+        }
+    }
+}
