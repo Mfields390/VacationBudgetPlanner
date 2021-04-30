d95bdca0d97bc4a416993e9c7f47bd81b5f0d4c4
Mfields390 <82343929+Mfields390@users.noreply.github.com>
2021-04-29 21:58:25 -0400

    week 3 update

--- Program.cs
+++ Program.cs
@@ -11,14 +11,32 @@ namespace VacationBudgetPlanner
             string name = Console.ReadLine();
             Console.WriteLine($"Hello {name}. Where would you like to travel to Mexico or Jamaica?");
             string destination = Console.ReadLine();
-            Console.WriteLine("How many days will you like to spend on your trip?");
-            int days = int.Parse (Console.ReadLine());
-            Console.WriteLine("How much spending money would you like to bring?");
-            Console.Write("$ ");
-            double money = double.Parse(Console.ReadLine());
-            double perDay = money / days;
-            Console.WriteLine($"Hey {name}, you are going to {destination} for {days} days with {money:c} for spending money. You can spend {perDay:c} per day.");
+            switch (destination.ToLower())
+            {
+                case "mexico":
+                    Console.WriteLine("How many days will you like to spend on your trip?");
+                    int mDays = int.Parse(Console.ReadLine());
+                    Console.WriteLine("How much spending money would you like to bring?");
+                    Console.Write("$ ");
+                    double mMoney = double.Parse(Console.ReadLine());
+                    double mPerDay = mMoney / mDays;
+                    Console.WriteLine($"Hey {name}, you are going to {destination} for {mDays} days with {mMoney:c} for spending money. You can spend {mPerDay:c} per day.");
+                    break;
+                case "jamaica":
+                    Console.WriteLine("How many days will you like to spend on your trip?");
+                    int jDays = int.Parse(Console.ReadLine());
+                    Console.WriteLine("How much spending money would you like to bring?");
+                    Console.Write("$ ");
+                    double jMoney = double.Parse(Console.ReadLine());
+                    double jPerDay = jMoney / jDays;
+                    Console.WriteLine($"Hey {name}, you are going to {destination} for {jDays} days with {jMoney:c} for spending money. You can spend {jPerDay:c} per day.");
+                    break;
+                default:
+                    Console.WriteLine("That's not one of your choices.");
+                    break;
 
+            }
+           
 
 
             Console.WriteLine("Please press enter to exit.");
